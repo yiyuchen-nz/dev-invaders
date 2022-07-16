d7bcfa0e030a62170f44adac74f06c572eab00be
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-07-16 16:51:51 +1200

    refactor code to make it object oriented

--- main.js
+++ main.js
@@ -1,7 +1,129 @@
-// import Preloader from './scripts/preloader';
-// import test from './scripts/preloader'
 
-// test()
+class Laser extends Phaser.Physics.Arcade.Sprite
+{
+	constructor(scene, x, y) {
+		super(scene, x, y, 'laser');
+	}
+
+	fire(x, y) {
+		this.body.reset(x, y);
+
+		this.setActive(true);
+		this.setVisible(false);
+
+		this.setVelocityY(-900);
+	}
+
+}
+
+class LaserGroup extends Phaser.Physics.Arcade.Group
+{
+	constructor(scene) {
+		super(scene.physics.world, scene);
+
+		this.createMultiple({
+			frameQuantity: 30,
+			key: 'laser',
+			active: false,
+			visible: false,
+			classType: Laser
+		});
+	}
+
+	fireBullet(x, y) {
+		const laser = this.getFirstDead(false);
+
+		if(laser) {
+			laser.fire(x, y);
+		}
+	}
+}
+
+class Laser extends Phaser.Physics.Arcade.Sprite
+{
+	constructor(scene, x, y) {
+		super(scene, x, y, 'laser');
+	}
+
+	fire(x, y) {
+		this.body.reset(x, y);
+
+		this.setActive(true);
+		this.setVisible(false);
+
+		this.setVelocityY(-900);
+	}
+
+}
+
+class LaserGroup extends Phaser.Physics.Arcade.Group
+{
+	constructor(scene) {
+		super(scene.physics.world, scene);
+
+		this.createMultiple({
+			frameQuantity: 30,
+			key: 'laser',
+			active: false,
+			visible: false,
+			classType: Laser
+		});
+	}
+
+	fireBullet(x, y) {
+		const laser = this.getFirstDead(false);
+
+		if(laser) {
+			laser.fire(x, y);
+		}
+	}
+}
+
+
+class SpaceScene extends Phaser.Scene{
+	constructor() {
+		super();
+    this.cursors
+    this.player
+    this.bullet
+    this.velocity = -50
+	}
+  
+  preload ()
+  {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('fireBall', 'assets/blast.png');
+    
+  }
+
+  create ()
+  {
+    this.add.image(400, 300, 'sky');
+
+    this.player = this.physics.add.sprite(20, 0, 'dude');
+    this.player.setCollideWorldBounds(true);
+
+    this.bullet = this.physics.add.sprite(0,0,'shoot')
+    this.bullet.setScale(0.05)
+    this.bullet.setCollideWorldBounds(true);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update ()
+  {
+    if (this.cursors.up.isDown)
+    {
+        this.velocity -= 10
+        this.player.setVelocityY(-200);
+    }
+    else {
+     this.velocity < 0 ? this.velocity += 10 : null
+    }
+  }
+
+}
 
 var config = {
   type: Phaser.AUTO,
@@ -10,48 +132,11 @@ var config = {
   physics: {
       default: 'arcade',
       arcade: {
-          gravity: { y: 300 },
+          gravity: { y: 330 },
           debug: false
       }
   },
-  scene: {
-      preload: preload,
-      create: create,
-      update: update
-  }
+  scene: SpaceScene
 };
 
-var player;
-var cursors;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-  this.load.image('sky', 'assets/sky.png');
-  this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-}
-
-function create ()
-{
-  this.add.image(400, 300, 'sky');
-
-  player = this.physics.add.sprite(0, 450, 'dude');
-  player.setCollideWorldBounds(true);
-
-  cursors = this.input.keyboard.createCursorKeys();
-}
-
-let velocity = -50
-
-function update ()
-{
-  if (cursors.up.isDown)
-  {
-      velocity -= 10
-      player.setVelocityY(velocity);
-  }
-  else {
-    velocity < 0 ? velocity += 10 : null
-  }
-}
+const game = new Phaser.Game(config);
