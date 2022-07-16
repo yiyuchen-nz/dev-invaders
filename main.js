abbda1ec108dae3e8d51e3dd32a93a5e46727484
David Thach <Davidcodes00@gmail.com>
2022-07-16 12:23:34 +1200

    changed resolution to widescreen

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1980,
+  height: 800,
   physics: {
     default: 'arcade',
     arcade: {
@@ -23,22 +23,4 @@ function preload() {
   this.load.image('red', 'assets/particles/red.png')
 }
 
-function create() {
-  this.add.image(400, 300, 'sky')
-
-  var particles = this.add.particles('red')
-
-  var emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD',
-  })
-
-  var logo = this.physics.add.image(400, 100, 'logo')
-
-  logo.setVelocity(100, 200)
-  logo.setBounce(1, 1)
-  logo.setCollideWorldBounds(true)
-
-  emitter.startFollow(logo)
-}
+function create() {}
