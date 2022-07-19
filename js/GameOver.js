17039efba53dd39c352835caf5c7975df8b60804
yiyuchen-nz <yiyuchen9@gmail.com>
2022-07-19 22:55:59 +1200

    add favicon to title and edited gameover page but replay still buuuuggggyy

--- js/GameOver.js
+++ js/GameOver.js
@@ -7,23 +7,25 @@ export default class GameOver extends Phaser.Scene {
 
   preload() {
     this.load.image('restart-game', '../assets/minipixel/restart.png')
+    this.load.image('game-over', '../assets/minipixel/UI-objects/GAME_OVER.png')
   }
 
   create() {
-    var text = this.add
-      .text(640, 360, 'Game Over', {
-        fontSize: 50,
+    this.add.sprite(640, 260, 'game-over').setScale(5)
+    this.add
+      .text(640, 360, 'Start Again', {
+        fontSize: 25,
         color: '#000000',
         fontStyle: 'bold',
       })
       .setOrigin(0.5)
 
     const restart = this.add
-      .sprite(670, 310, 'restart-game')
+      .sprite(760, 360, 'restart-game')
       .setScale(0.1)
       .setInteractive()
 
-    var mainScene = this.scene.get('MainScene')
+    //var mainScene = this.scene.get('MainScene')
     restart.on('pointerdown', () => this.scene.start('Title'))
     // mainScene.scene.restart())
     //  this.scene.restart('MainScene'))
